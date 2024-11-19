6ed06164fbe4ce6a0fa6e570feee238ccec431f2
VaNkOSl <ivan_petrov0341@abv.bg>
2024-11-19 16:31:06 +0200

    Updating Home Page

---
+++ FarmFresh/wwwroot/js/animations.js
@@ -0,0 +1,33 @@
+﻿AOS.init();
+
+window.onscroll = function () {
+    let button = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+document.getElementById("scrollToTopBtn").onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
+$('a[href^="#"]').on('click', function (event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
+
+const fadeUpElements = document.querySelectorAll('.fade-up');
+window.addEventListener('scroll', () => {
+    fadeUpElements.forEach(el => {
+        if (el.getBoundingClientRect().top < window.innerHeight) {
+            el.classList.add('visible');
+        }
+    });
+});
