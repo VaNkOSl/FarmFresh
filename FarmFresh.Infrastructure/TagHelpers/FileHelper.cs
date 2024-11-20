a5803f7e5dd4593a075d3e804019235fb3bb5eca
tilkov03 <tilkovpetko40@gmail.com>
2024-11-20 19:24:52 +0200

    Creating FileHelper

---
+++ FarmFresh.Infrastructure/TagHelpers/FileHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmFresh.Infrastructure.TagHelpers
+{
+    public static class FileHelper
+    {
+        public static async Task<byte[]> ReadFileAsync(IFormFile file)
+        {
+            if (file.Length == 0) throw new ArgumentException("File is empty.");
+
+            using var memoryStream = new MemoryStream();
+            await file.CopyToAsync(memoryStream);
+
+            return memoryStream.ToArray();
+        }
+
+        public static async Task SaveFileAsync(byte[] fileData, string path)
+        {
+            await File.WriteAllBytesAsync(path, fileData);
+        }
+
+        public static void DeleteFile(string path)
+        {
+            if (File.Exists(path)) File.Delete(path);
+        }
+    }
+}
