55ae22015d0c0fdd48f5112eae3c6ae12ecb5eb3
VaNkOSl <ivan_petrov0341@abv.bg>
2024-11-30 17:08:51 +0200

    Split the login mapping in the Mapping Profile into different classes.

---
+++ FarmFresh/Mapper/CategoryMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FarmFresh.Data.Models;
+using FarmFresh.ViewModels.Category;
+
+namespace FarmFresh.Mapper;
+
+public class CategoryMappingProfile : Profile
+{
+    public CategoryMappingProfile()
+    {
+        CreateMap<CategoryCreateForm, Category>();
+        CreateMap<Category, CategoryUpdateForm>()
+            .ReverseMap();
+        CreateMap<CategoryUpdateForm, Category>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
+    }
+}
---
+++ FarmFresh/Mapper/FarmerLocationMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FarmFresh.Data.Models;
+using FarmFresh.ViewModels.Farmer;
+
+namespace FarmFresh.Mapper;
+
+public class FarmerLocationMappingProfile : Profile
+{
+    public FarmerLocationMappingProfile()
+    {
+        CreateMap<FarmerLocationDto, FarmerLocation>()
+            .ForMember(dest => dest.FarmerId, opt => opt.Ignore());
+    }
+}
