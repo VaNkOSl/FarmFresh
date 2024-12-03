8bec953848b8373b243143dacabdce152c2e3826
david20033 <daviddimitrov203@gmail.com>
2024-12-03 17:08:22 +0200

    Fixed CartItem ViewModel ProductId type from int to Guid

--- FarmFresh.ViewModels/Order/CartItemViewModel.cs
+++ FarmFresh.ViewModels/Order/CartItemViewModel.cs
@@ -8,7 +8,7 @@ namespace FarmFresh.ViewModels.Order
 {
     public class CartItemViewModel
     {
-        public int ProductId { get; set; }
+        public Guid ProductId { get; set; }
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
